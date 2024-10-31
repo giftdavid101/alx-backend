df45640229c410fa8273349e16557e2f77625619
giftdavid101 <giftdavid101@gmail.com>
2024-10-31 05:39:27 +0100

    1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -2,7 +2,7 @@
 
 '''FIFO caching'''
 
-from collections import OrderedDict
+from threading import RLock
 from base_caching import BaseCaching
 
 
@@ -13,23 +13,23 @@ class FIFOCache(BaseCaching):
 
     def __init__(self):
         super().__init__()
-        self.cache_data = OrderedDict()
+        self.__keys = []
+        self.__rlock = RLock()
 
     def put(self, key, item):
         '''assign to the dictionary `self.cache_data` the
            `item` value for the key `key`
         '''
 
-        if key is None or item is None:
-            return
-
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key, _ = self.cache_data.popitem(last=False)
-            print(f"DISCARD: {first_key}")
-
-        self.cache_data[key] = item
+        if key is not None and item is not None:
+            keyOut = self._balance(key)
+            with self.__rlock:
+                self.cache_data.update({key: item})
+            if keyOut is not None:
+                print('DISCARD: {}'.format(keyOut))
 
     def get(self, key):
         '''return the value in `self.cache_data` linked to `key`
         '''
-        return self.cache_data.get(key, None)
+        with self.__rlock:
+            return self.cache_data.get(key, None)
