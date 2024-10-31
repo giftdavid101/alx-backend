4044d2763ef55d916525e7102c24ee6c80feb95a
giftdavid101 <giftdavid101@gmail.com>
2024-10-31 05:40:55 +0100

    1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -33,3 +33,16 @@ class FIFOCache(BaseCaching):
         '''
         with self.__rlock:
             return self.cache_data.get(key, None)
+   
+   def _balance(self, keyIn):
+        """ Removes the oldest item from the cache at MAX size
+        """
+        keyOut = None
+        with self.__rlock:
+            if keyIn not in self.__keys:
+                keysLength = len(self.__keys)
+                if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                    keyOut = self.__keys.pop(0)
+                    self.cache_data.pop(keyOut)
+                self.__keys.insert(keysLength, keyIn)
+        return keyOut
